50927844e5927565d270bc2ea1acda44c6dba9b3
Sweets Sweetman <sweetmantech@gmail.com>
2023-01-03 12:11:18 -0500

    I init API to upload DALL-E images to IPFS for decentralized file storage.

---
+++ lib/getHostedFile.ts
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+const getHostedFile = async (httpsUri: string) => {
+  const response = await fetch(httpsUri);
+  const blob = await response.blob();
+  return blob;
+};
+
+export default getHostedFile;
